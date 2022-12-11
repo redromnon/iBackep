a7803f87b546d82fb526a9992c85da2fdc4bf944
Redromnon <rishimoharir@gmail.com>
2022-12-11 19:22:14 +0530

    [UI] Add encrypt help dialog

--- src/ui/encrypt.py
+++ src/ui/encrypt.py
@@ -10,11 +10,30 @@ class Encrypt(ft.UserControl):
         )
 
         self.password_field = ft.TextField(
-            password=True, can_reveal_password=True, disabled=True, width=275, height=40, 
+            password=True, can_reveal_password=True, disabled=True, width=270, height=40, 
             content_padding=ft.padding.only(bottom=15, left=10), hint_text="Encryption password"
         )
 
-        return ft.Row([self.encrypt_checkbox, self.password_field], spacing=10, alignment="center")
+        self.info_button = ft.IconButton(
+            icon=ft.icons.INFO_ROUNDED, icon_size=28, on_click=self.toggle_help
+        )
+
+        self.encrypt_help = ft.AlertDialog(
+            title=ft.Text("Encryption Help", text_align="center"),
+            content=ft.Column(
+                [
+                    ft.Text("Your backup can be protected by encrypting it with a password. Just enable the checkbox and type your password.\n\n" +
+                            "When performing a BACKUP, you need to set a password and encrypt it for the first time only." +
+                            " There's no need to insert the password every time once it's already encrypted.\n\n" +
+                            "When performing a RESTORE however, you will need to insert the password every single time.",
+                            text_align="center", style="bodySmall"),
+                ],
+                horizontal_alignment="center", height=120, width=500
+            ),
+            actions=[ft.TextButton("Ok", on_click=self.toggle_help)], modal=True 
+        )
+
+        return ft.Row([self.encrypt_checkbox, self.password_field, self.info_button, self.encrypt_help], spacing=10, alignment="center")
 
     
     def enable_checkbox(self,e):
@@ -36,3 +55,12 @@ class Encrypt(ft.UserControl):
             if len(self.password_field.value) > 0:
                 
                 return self.password_field.value
+
+    def toggle_help(self, e):
+
+        if self.encrypt_help.open:
+            self.encrypt_help.open=False
+        else:
+            self.encrypt_help.open=True
+        
+        self.update()
